85e8e49be5c804cd53e78599e98d1e0d63c934df
Eoin McCarthy <em1081@my.bristol.ac.uk>
2018-12-06 12:59:58 +0200

    add request wrapper component
    related #4

---
+++ client/src/components/Request.js
@@ -0,0 +1,52 @@
+import { Component } from 'react'
+import axios from 'axios'
+
+const PENDING = 'PENDING'
+const ERROR = 'ERROR'
+const DONE = 'DONE'
+
+export default class Request extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      requestStatus: PENDING
+    }
+  }
+
+  componentDidMount() {
+    axios[this.props.method](this.props.url)
+      .then(response => {
+        this.setState({
+          payload: response.data,
+          requestStatus: DONE
+        })
+      }, error => {
+        this.setState({
+          error,
+          requestStatus: ERROR
+        })
+      })
+  }
+
+  render() {
+    const {
+      state: {
+        requestStatus,
+        payload,
+        error
+      },
+      props: {
+        renderPending,
+        renderError,
+        children
+      }
+    } = this
+
+    if (requestStatus === PENDING) return renderPending()
+
+    if (requestStatus === ERROR) return renderError(error)
+
+    return children(payload)
+  }
+}
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('env2')('.env') // configure enviroment (in ./.env)
 
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
 app.use(express.static('client/build'))
+app.use(morgan('tiny'))
 app.use(require('./routers/airtable'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
