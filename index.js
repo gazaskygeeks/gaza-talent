d9c0fca988b147afc4fd74aeb02f07583c852e20
Eoin McCarthy <em1081@my.bristol.ac.uk>
2018-12-06 18:12:26 +0200

    joins profiles data to people data

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-require("env2")(".env"); // configure enviroment (in ./.env)
-
 const express = require("express");
 const morgan = require("morgan");
 
+// INIT
+require("env2")(".env"); // configure enviroment (in ./.env)
+
 const app = express();
 
+// MIDDLEWARE
+app.use(morgan("tiny"));
+
+// CLIENT
+app.use(express.static("client/build"));
+
+// API
+const apiRouter = new express.Router();
+
+apiRouter.get("/talent", require("./controllers/getTalent.js"));
+
+app.use("/api", apiRouter);
+
+// START
 const port = process.env.PORT || 3000;
 
-app.use(express.static("client/build"));
-app.use(morgan("tiny"));
-app.use("/api", require("./routers/airtable"));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
