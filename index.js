3beb03a8a5c17fdf41355df7645211fb61a3ec2a
Eoin McCarthy <em1081@my.bristol.ac.uk>
2018-12-06 10:49:51 +0200

    adds /talent endpoint
      * sets up airtable
      * /talent endpoint fetches all developers
    
    related #1 #2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('env2')('.env') // configure enviroment (in ./.env)
+
 const express = require('express')
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('client/build'))
+app.use(require('./routers/airtable'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routers/airtable.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const Airtable = require('airtable')
+
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_API_KEY
+})
+
+const tableName = 'Freelancers Cohort 4 & 5 Availability'
+
+const base = Airtable.base('appgBOlw7B0N1kQiq')
+const router = new express.Router()
+
+const onNextPage = recordPrev => (records, fetchNextPage) => {
+  records.forEach(record => {
+    recordPrev.push(record)
+  })
+
+  fetchNextPage()
+}
+
+const onDone = (res, records) => err => {
+  if (err) {
+    console.error(err)
+    return res.notFound()
+  }
+
+  return res.json(records)
+}
+
+router.get('/talent', (req, res) => {
+  let totalRecords = []
+
+  base(tableName).select({
+    view: 'Grid view'
+  }).eachPage(
+    onNextPage(totalRecords),
+    onDone(res, totalRecords)
+  )
+})
+
+module.exports = router
