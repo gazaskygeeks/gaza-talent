e20d2ed9bc4cdc9e94ce3bd22f950c94371892db
maxgerber <mgerber94@gmail.com>
2018-12-06 15:08:33 +0200

    + Create ListingItemCreatedAt Component with moment library
    Relates #8

---
+++ client/src/constants/airtableKeys.js
@@ -0,0 +1,2 @@
+export const UNTIL_AVAILABLE =
+  "If you're not available now, when will you be available ?";
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
+import moment from "moment";
 import styled from "styled-components";
+import { UNTIL_AVAILABLE } from "../constants/airtableKeys";
 import * as r from "ramda";
 
 const HomeContainer = styled.section.attrs({
@@ -18,6 +20,13 @@ const ListingItemNameText = styled.h3.attrs({
   className: "f3 b"
 })``;
 
+const ListingItemAvailability = ({ listing }) => {
+  const listingCreatedAt = moment(listing._rawJson.createdTime).format(
+    "DD MMMM"
+  );
+  return <section>Listing Created At: {listingCreatedAt}</section>;
+};
+
 const Home = ({ listings }) => {
   return (
     <HomeContainer>
@@ -25,6 +34,7 @@ const Home = ({ listings }) => {
         {r.map(listing => (
           <ListingItem key={listing.id}>
             <ListingItemNameText>{listing.fields.Name}</ListingItemNameText>
+            <ListingItemAvailability listing={listing} />
           </ListingItem>
         ))(listings)}
       </ListingItemList>
